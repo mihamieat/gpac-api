0efee8ed4bb5df145f049daa78450126ba0e6076
Mihamina Rakotovazaha <r.miham@yahoo.com>
2024-08-19 12:07:12 +0200

    refactor: introduce fixture to db settings

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,10 +2,47 @@
 """Test the main module."""
 
 import os
+
+from bson import ObjectId
+import pytest
+from fastapi.testclient import TestClient
+
+from src.gpac_api.app.main import app
 from src.gpac_api.app.db.database import get_database
 
 os.environ["TESTING"] = "1"
 db = get_database()
 
+
+@pytest.fixture
+def client():
+    """
+    Provides a test client for the FastAPI application.
+
+    Returns:
+        TestClient: An instance of the FastAPI test client.
+    """
+
+    return TestClient(app)
+
+
 # notifs
-notif_collection = db["notif"]
+@pytest.fixture
+def notif_collection():
+    """
+    Sets up a notification collection in the database for testing purposes.
+
+    Returns:
+        Collection: The notification collection from the database.
+    """
+
+    notif_collection_db = db["notif"]
+    notif_collection_db.insert_one(
+        {
+            "_id": ObjectId(1),
+            "recipients": ["string"],
+            "gpus": ["string"],
+            "mail_content": "string",
+            "message_type": "info",
+        }
+    )
--- tests/main_test.py
+++ tests/main_test.py
@@ -1,13 +1,8 @@
 # -*- coding: utf-8 -*-
 """Test the main module."""
-from fastapi.testclient import TestClient
-from src.gpac_api.app.main import app
 
 
-client = TestClient(app)
-
-
-def test_read_main():
+def test_read_root(client):
     """
     Raises:
         AssertionError: If the response status code is not 200.
