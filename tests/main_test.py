b3d74df5de4cd54f16a8661717afa840fcc8cceb
Mihamina Rakotovazaha <r.miham@yahoo.com>
2024-08-19 10:01:39 +0200

    test: add pytest template

---
+++ tests/main_test.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""Test the main module."""
+
+from fastapi.testclient import TestClient
+from src.gpac_api.app.main import app
+
+
+client = TestClient(app)
+
+
+def test_read_main():
+    """
+    Raises:
+        AssertionError: If the response status code is not 200.
+    """
+    response = client.get("/")
+    assert response.status_code == 200
