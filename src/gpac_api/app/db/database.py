35eac1f9b41ddad20ade6fd75b50feec712a87a9
Mihamina Rakotovazaha <r.miham@yahoo.com>
2024-08-19 11:42:02 +0200

    test: switch test database while doing test

--- src/gpac_api/app/db/database.py
+++ src/gpac_api/app/db/database.py
@@ -1,19 +1,28 @@
 # -*- coding: utf-8 -*-
 """Database module"""
+import os
 
+from dotenv import load_dotenv, dotenv_values
 from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo import MongoClient
 
+load_dotenv()
 
-DATABASE_USER = "rmiham"
-DATABASE_PASSWORD = "NAdKy4ljB52FhRvw"
-DATABASE_DOMAIN = "atlasprime.8ldgp.mongodb.net"
-DATABASE_APP_NAME = "AtlasPrime"
-DATABASE_URL = f"mongodb+srv://{DATABASE_USER}:{DATABASE_PASSWORD}\
+if os.environ.get("TESTING") not in ("true", "1", "yes"):
+    config = dotenv_values()
+    DATABASE_USER = config.get("DATABASE_USER")
+    DATABASE_PASSWORD = config.get("DATABASE_PASSWORD")
+    DATABASE_DOMAIN = config.get("DATABASE_DOMAIN")
+    DATABASE_APP_NAME = config.get("DATABASE_APP_NAME")
+    DATABASE_URL = f"mongodb+srv://{DATABASE_USER}:{DATABASE_PASSWORD}\
 @{DATABASE_DOMAIN}/?retryWrites=true&w=majority&appName={DATABASE_APP_NAME}"
-DATABASE_CLIENT = "gpac_db_test"
+    DATABASE_CLIENT = config.get("DATABASE_CLIENT")
 
-client = AsyncIOMotorClient(DATABASE_URL)
-db = client[DATABASE_CLIENT]
+    client = AsyncIOMotorClient(DATABASE_URL)
+    db = client[DATABASE_CLIENT]
+else:
+    client = MongoClient("mongodb://tester:tester$@localhost:27017")
+    db = client["testdb"]
 
 
 def get_database():
