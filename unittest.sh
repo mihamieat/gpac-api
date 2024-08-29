66cbd086c7d81740b6dc3d6fd70a702c57c251e6
Mihamina Rakotovazaha <r.miham@yahoo.com>
2024-08-29 19:44:11 +0200

    ci: add codecov support

--- unittest.sh
+++ unittest.sh
@@ -2,6 +2,6 @@
 
 docker-compose -f docker-compose.test.yaml down
 docker-compose -f docker-compose.test.yaml up -d
-pytest --cov=src tests/
+poetry run pytest --cov=src --cov-report=xml
 docker-compose -f docker-compose.test.yaml down
 coverage html
