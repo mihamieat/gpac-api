8ac221654228d37a1343d56bb27946fcb7804aa7
Mihamina Rakotovazaha <r.miham@yahoo.com>
2025-02-13 16:42:23 +0100

    feat(ci): add automatic version bump #31

---
+++ src/gpac_api/app/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+"""Project app package."""
+__version__ = "0.1.0-dev"
---
+++ src/gpac_api/app/crud/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+"""App crud operations package."""
+__version__ = "0.1.0-dev"
