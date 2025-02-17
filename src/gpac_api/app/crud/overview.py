0663a59f861065e06d833b55dd6c0da9d9f9ba7f
Mihamina Rakotovazaha <r.miham@yahoo.com>
2025-02-17 17:07:21 +0100

    feat: add custom hostname list endpoint #39

--- src/gpac_api/app/crud/overview.py
+++ src/gpac_api/app/crud/overview.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 """handle CRUD operations for overview data model."""
 from typing import Optional
+
+from fastapi import HTTPException
+
 from src.gpac_api.app.schemas.overview import (
     OverviewCreateSchema,
     OverviewResponseSchema,
@@ -56,3 +59,42 @@ def get_all_hostnames() -> HostnamesResponseSchema:
     collection = db.overview_data
     results = list(collection.distinct("hostname"))
     return HostnamesResponseSchema(hostnames=results)
+
+
+def add_to_custom_hostname_list(hostname: str) -> HostnamesResponseSchema:
+    """Adds a hostname to a custom list in the database."""
+
+    if hostname not in get_all_hostnames().hostnames:
+        raise HTTPException(status_code=404, detail="Invalid hostname")
+
+    collection = db.custom_hostnames
+    if not collection.find_one({"hostname": hostname}):
+        document = {"hostname": hostname}
+        document["_id"] = collection.insert_one(document).inserted_id
+
+    return HostnamesResponseSchema(hostnames=[hostname])
+
+
+def get_custom_hostname_list() -> HostnamesResponseSchema:
+    """
+    Retrieves the custom hostname list from the database.
+
+    Returns:
+        HostnamesResponseSchema
+    """
+    collection = db.custom_hostnames
+    results = list(collection.find())
+    return HostnamesResponseSchema(hostnames=[result["hostname"] for result in results])
+
+
+def del_hostname_from_custom_list(hostname: str) -> HostnamesResponseSchema:
+    """
+    Removes a hostname from the custom list in the database.
+
+    Returns:
+        HostnamesResponseSchema
+    """
+    collection = db.custom_hostnames
+    if not collection.find_one({"hostname": hostname}):
+        raise HTTPException(status_code=404, detail="Invalid hostname")
+    collection.delete_one({"hostname": hostname})
--- tests/hostname_test.py
+++ tests/hostname_test.py
@@ -1,14 +1,100 @@
 # -*- coding: utf-8 -*-
 """Test overview/hostname-list endpoint."""
 
-ENDPOINT = "/overview/hostname-list"
+ENDPOINT = "/overview"
 
 
 def test_get_all_hostnames(client, headers):
     """Test get_all_hostnames."""
-    response = client.get(ENDPOINT, headers=headers)
+    response = client.get(f"{ENDPOINT}/hostname-list", headers=headers)
     response_data = response.json()
 
     assert response.status_code == 200
     assert isinstance(response_data["hostnames"], list)
     assert all(isinstance(hostname, str) for hostname in response_data["hostnames"])
+
+
+def test_add_hostname_in_custom_list(client, headers, overview_data_db):
+    """Test add_hostname_in_custom_list"""
+    _ = overview_data_db
+    hostname = "server1"
+    response = client.post(
+        f"{ENDPOINT}/hostname/custom?hostname={hostname}",
+        headers=headers,
+        json={"hostname": hostname},
+    )
+    response_data = response.json()
+
+    assert response.status_code == 200
+    assert "hostnames" in response_data
+    assert hostname in response_data["hostnames"]
+
+
+def test_add_existing_hostname_in_custom_list(client, headers, overview_data_db):
+    """Test add_existing_hostname_in_custom_list"""
+    _ = overview_data_db
+    hostname = "server1"
+    response = client.post(
+        f"{ENDPOINT}/hostname/custom?hostname={hostname}",
+        headers=headers,
+        json={"hostname": hostname},
+    )
+    response = client.post(
+        f"{ENDPOINT}/hostname/custom?hostname={hostname}",
+        headers=headers,
+        json={"hostname": hostname},
+    )
+    response_data = response.json()
+
+    assert response.status_code == 200
+    assert "hostnames" in response_data
+    assert len(response_data["hostnames"]) == 1
+    assert response_data["hostnames"][0] == hostname
+
+
+def test_add_non_existing_hostname_in_custom_list(client, headers, overview_data_db):
+    """Test add_non_existing_hostname_in_custom_list."""
+    _ = overview_data_db
+    hostname = "non_existing_server"
+    response = client.post(
+        f"{ENDPOINT}/hostname/custom?hostname={hostname}",
+        headers=headers,
+        json={"hostname": hostname},
+    )
+    response_data = response.json()
+
+    assert response.status_code == 404
+    assert "detail" in response_data
+    assert response_data["detail"] == "Invalid hostname"
+
+
+def test_get_custom_hostname_list(client, headers, overview_data_db):
+    """Test get_custom_hostname_list."""
+    _ = overview_data_db
+    response = client.get(f"{ENDPOINT}/hostname/custom", headers=headers)
+    response_data = response.json()
+
+    assert response.status_code == 200
+    assert "hostnames" in response_data
+    assert len(response_data["hostnames"]) > 0
+
+
+def test_delete_custom_hostname_list(client, headers, overview_data_db):
+    """Test delete_custom_hostname_list."""
+    _ = overview_data_db
+    hostname = "server1"
+    response = client.delete(f"{ENDPOINT}/hostname/custom/{hostname}", headers=headers)
+
+    assert response.status_code == 200
+
+
+def test_delete_unexisting_custom_hostname_list(client, headers, overview_data_db):
+    """Test delete_unexisting_custom_hostname_list."""
+    _ = overview_data_db
+    hostname = "server2"
+    response = client.delete(f"{ENDPOINT}/hostname/custom/{hostname}", headers=headers)
+    response_data = response.json()
+
+    assert response.status_code == 404
+    assert "detail" in response_data
+    assert response_data["detail"] == "Invalid hostname"
