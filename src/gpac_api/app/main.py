81f97503d5e9989167be3efbf018591c34ce608f
Mihamina Rakotovazaha <r.miham@yahoo.com>
2025-02-19 11:45:52 +0100

    fix: enable CORS supoort for configured origins #42

--- src/gpac_api/app/main.py
+++ src/gpac_api/app/main.py
@@ -1,12 +1,31 @@
 # -*- coding: utf-8 -*-
 """Gpac API main module."""
 
+import os
+
+from dotenv import load_dotenv, dotenv_values
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from src.gpac_api.app.utils.lifespan import lifespan
 from src.gpac_api.app.api.endpoints import notif, gpu_data, overview
 
 
+load_dotenv()
+dotenv_config = dotenv_values()
+FRONTEND_ORIGIN = os.getenv("FRONTEND_ORIGIN", dotenv_config.get("FRONTEND_ORIGIN"))
+
 app = FastAPI(lifespan=lifespan)
+
+origins = list(filter(None, ["http://localhost:3000", FRONTEND_ORIGIN]))
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(notif.router, prefix="/notif", tags=["notif"])
 app.include_router(gpu_data.router, prefix="/gpu_data", tags=["gpu_data"])
